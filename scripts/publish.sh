a195303fab4c74787f602991ee208dc2c97a8ef2
Stephen Hicks <stephenhicks@gmail.com>
2019-01-11 23:28:00 -0800

    publish permanent versions

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,32 +4,40 @@ shopt -s nullglob
 
 # Publish to github-pages
 
-# Start by checking out the repo in a subdirectory
+# Make sure there are not pending changes
+if git diff | grep -q diff; then
+  git status >&2
+  echo "Refusing to publish with pending changes." >&2
+  exit 1
+fi
 
+# Start by checking out the repo in a subdirectory
 HASH=$(git rev-parse HEAD | head -c 7)
 DATE=$(date)
 TMP=$(mktemp -d)
+DIR="$TMP/r/$HASH"
 (
-  cd $TMP
+  cd "$TMP"
   git clone --depth=1 git@github.com:shicks/crystalis-randomizer r -b gh-pages
   cd r
-  rm -rf *
-  mkdir -p view
+  mkdir -p "$HASH/view"
 )
 (
-  cp notes/depgraph.svg $TMP/r/
-  cp notes/locations.svg $TMP/r/
-  cp notes/traversal.txt $TMP/r/
+  cp -f notes/depgraph.svg "$TMP/r/"
+  cp -f notes/locations.svg "$TMP/r/"
+  cp -f notes/traversal.txt "$TMP/r/"
   cd src
-  cp *.js *.css $TMP/r
-  cp view/*.js view/*.css $TMP/r/view/
+  cp *.js *.css "$DIR"
+  cp view/*.js view/*.css "$DIR/view/"
   # TODO - add a datestamp or commit stamp into the HTML somehow
   #      - maybe use sed to replace a placeholder?
   for a in *.html view/*.html; do
-    cat ga.tag $a >| $TMP/r/$a
+    cat ga.tag $a >| "$DIR/$a"
   done
   sed -e "/BUILD_HASH/ s/latest/$HASH/" -e "/BUILD_DATE/ s/current/$DATE/" \
-      patch.js >| $TMP/r/patch.js
+      patch.js >| "$DIR/patch.js"
+  echo "<a href=\"$HASH/\">$HASH: $DATE</a><br>" >> "$TMP/r/versions.html"
+  sed "s,main.js,$HASH/main.js,g" index.html >| "$TMP/r/versions.html"
 )
 (
   cd $TMP/r

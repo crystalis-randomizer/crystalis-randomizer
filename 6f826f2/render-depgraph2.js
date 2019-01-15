6600e38c0327eaac3985c920fc393fe9d9add845
Stephen Hicks <stephenhicks@gmail.com>
2019-01-14 19:05:22 -0800

    remove broken versions

--- 6f826f2/render-depgraph2.js
+++
@@ -1,9 +0,0 @@
-require = require('esm')(module);
-
-const {generate} = require('./depgraph2.js');
-const Viz = require('viz.js');
-const {Module, render} = require('viz.js/full.render.js');
-
-const viz = new Viz({Module: () => Module({TOTAL_MEMORY: 1<<25}), render});
-viz.renderString(generate().toLocationGraph()).then(console.log);
-//console.log(graph.toLocationGraph());

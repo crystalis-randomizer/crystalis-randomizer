ff3201da95aee6a5048305a415e106f088bf2087
Travis CI User <travis@example.org>
2019-02-25 22:09:27 +0000

    Release 1.0.0-rc1

--- 1.0.0-rc1/js/version.js
+++ 1.0.0-rc1/js/version.js
@@ -1,5 +1,5 @@
 export const STATUS = 'rc';
 export const VERSION = '1.0.0-rc1';
 export const LABEL = '1.0.0-rc1';
-export const HASH = 'e390d7572d3c5ea28e597befd34c00e1d0683bae';
-export const DATE = new Date(1551126571000);
+export const HASH = 'b934130c095d348b007cd349d16c7ea982e2b517';
+export const DATE = new Date(1551132554000);

86cfef9782d5bade9a1be8679bf413191f4a10f2
Travis CI User <travis@example.org>
2019-02-26 11:38:06 +0000

    Release assumed

---
+++ assumed/js/bits.js
@@ -0,0 +1,64 @@
+import {Bits as BitsBigint} from './bits_bigint.js';
+import {Bits as BitsArray} from './bits_array.js';
+
+const b = BitsBigint.BIGINT_OK ? BitsBigint : BitsArray;
+
+export class Bits {};
+
+/**
+ * @param {...number} nums
+ * @return {!Bits}
+ */
+Bits.of = /** @type {?} */ (b.of);
+
+/**
+ * @param {!Iterable<number>} nums
+ * @return {!Bits}
+ */
+Bits.from = /** @type {?} */ (b.from);
+
+/**
+ * @param {!Bits} superset
+ * @param {!Bits} subset
+ * @return {boolean}
+ */
+Bits.containsAll = /** @type {?} */ (b.containsAll);
+
+/**
+ * @param {!Bits} bits
+ * @param {number} num
+ * @return {!Bits}
+ */
+Bits.with = /** @type {?} */ (b.with);
+
+/**
+ * @param {!Bits} bits
+ * @param {number} num
+ * @return {!Bits}
+ */
+Bits.without = /** @type {?} */ (b.without);
+
+/**
+ * @param {!Bits} bits
+ * @param {number} num
+ * @return {boolean}
+ */
+Bits.has = /** @type {?} */ (b.has);
+
+/**
+ * @param {!Bits} bits
+ * @return {!Array<number>}
+ */
+Bits.bits = /** @type {?} */ (b.bits);
+
+/**
+ * @param {!Bits} bits
+ * @return {!Bits}
+ */
+Bits.clone = /** @type {?} */ (b.clone);
+
+/**
+ * @param {!Bits} bits
+ * @return {boolean}
+ */
+Bits.empty = /** @type {?} */ (b.empty);
---
+++ assumed/js/version.js
@@ -0,0 +1,5 @@
+export const STATUS = 'unstable';
+export const VERSION = 'assumed';
+export const LABEL = 'assumed 1c33399';
+export const HASH = '1c3339970b1c14a1919a38fe2f2fd41bfafd0d59';
+export const DATE = new Date(1551181073000);

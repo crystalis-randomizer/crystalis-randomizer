44065fcd4f84010b916325e4c00ccf567593f01b
Stephen Hicks <stephenhicks@gmail.com>
2019-03-11 12:36:09 -0700

    delete "assumed" and last hashed branch

--- 138d4fe/js/traverse-depgraph2.js
+++
@@ -1,8 +0,0 @@
-require = require('esm')(module);
-
-const {generate} = require('./depgraph2.js');
-const start = new Date().getTime();
-const {win, path} = generate().traverse();
-console.log(path.join('\n'));
-const end = new Date().getTime();
-console.log(`time: ${end - start} ms`);
--- assumed/js/version.js
+++
@@ -1,5 +0,0 @@
-export const STATUS = 'unstable';
-export const VERSION = 'assumed';
-export const LABEL = 'assumed 956f677';
-export const HASH = '956f677e0672325716f60bd5e8c257b611fe9edd';
-export const DATE = new Date(1551217271000);

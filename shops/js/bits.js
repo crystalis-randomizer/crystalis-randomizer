cfbf051d75c8f0bc5c9189c581a6634e231da8aa
Travis CI User <travis@example.org>
2019-03-27 22:31:14 +0000

    Release shops

---
+++ shops/js/bits.js
@@ -0,0 +1,71 @@
+import {BitsBigInt, BIGINT_OK} from './bits_bigint.js';
+import {BitsArray} from './bits_array.js';
+
+const b = BIGINT_OK ? BitsBigInt : BitsArray;
+
+export class Bits {};
+
+/**
+ * @param {...number} nums
+ * @return {!Bits}
+ */
+Bits.of = /** @type {?} */ (b.of);
+
+/**
+ * @param {!Iterable<number>} nums
+ * @return {!Bits}
+ */
+Bits.from = /** @type {?} */ (b.from);
+
+/**
+ * @param {!Bits} superset
+ * @param {!Bits} subset
+ * @return {boolean}
+ */
+Bits.containsAll = /** @type {?} */ (b.containsAll);
+
+/**
+ * @param {!Bits} bits
+ * @param {number} num
+ * @return {!Bits}
+ */
+Bits.with = /** @type {?} */ (b.with);
+
+/**
+ * @param {!Bits} bits
+ * @param {number} num
+ * @return {!Bits}
+ */
+Bits.without = /** @type {?} */ (b.without);
+
+/**
+ * @param {!Bits} bits
+ * @param {number} num
+ * @return {boolean}
+ */
+Bits.has = /** @type {?} */ (b.has);
+
+/**
+ * @param {!Bits} bits
+ * @return {!Array<number>}
+ */
+Bits.bits = /** @type {?} */ (b.bits);
+
+/**
+ * @param {!Bits} bits
+ * @return {!Bits}
+ */
+Bits.clone = /** @type {?} */ (b.clone);
+
+/**
+ * @param {!Bits} bits
+ * @return {boolean}
+ */
+Bits.empty = /** @type {?} */ (b.empty);
+
+/**
+ * @param {!Bits} left
+ * @param {!Bits} right
+ * @return {!Bits}
+ */
+Bits.difference = /** @type {?} */ (b.difference);

6512fb5c0b9f50cb173c5555daa9533f610a4fd3
Travis CI User <travis@example.org>
2019-04-18 08:50:55 +0000

    Release latest

--- latest/js/version.js
+++ latest/js/version.js
@@ -1,5 +1,5 @@
 export const STATUS = 'unstable';
 export const VERSION = 'latest';
-export const LABEL = 'latest fb20979';
-export const HASH = 'fb20979a3a18904a7c7a70409d711795e10c190c';
-export const DATE = new Date(1555401497000);
+export const LABEL = 'latest 4fc6254';
+export const HASH = '4fc62549b5b184d8259a39abb00f0e9acd3b4953';
+export const DATE = new Date(1555577445000);

71797f914850cf5b391ec9cbeab3a5c1c6609464
Travis CI User <travis@example.org>
2019-04-28 11:13:25 +0000

    Release defrag

--- defrag/js/util.js
+++ defrag/js/util.js
@@ -1,226 +1,68 @@
-/** @template T */
 export class Deque {
-  constructor() {
-    /** @type {!Array<T|undefined>} */
-    this.buffer = new Array(16);
-    /** @type {number} */
-    this.mask = 0xf;
-    /** @type {number} */
-    this.start = 0;
-    /** @type {number} */
-    this.end = 0;
-    /** @type {number} */
-    this.size = 0; // readonly externally
-  }
-
-  get length() { return this.size; } // TODO - just use length
-
-  upsize(target) {
-    while (this.mask < target) {
-      if (this.end <= this.start) this.start += this.mask + 1;
-      this.mask = this.mask << 1 | 1;
-      this.buffer = this.buffer.concat(this.buffer);
+    constructor() {
+        this.buffer = new Array(16);
+        this.mask = 0xf;
+        this.start = 0;
+        this.end = 0;
+        this.size = 0;
     }
-    this.size = target;
-  }
-
-  /** @param {...T} elem */
-  push(...elems) {
-    this.upsize(this.size + elems.length);
-    for (const elem of elems) {
-      this.buffer[this.end] = elem;
-      this.end = (this.end + 1) & this.mask;
+    get length() {
+        return this.size;
     }
-  }
-
-  /** @return {T|undefined} */
-  pop() {
-    if (!this.size) return undefined;
-    this.end = (this.end - 1) & this.mask;
-    this.size--;
-    return this.buffer[this.end];
-  }
-
-  /** @return {T|undefined} */
-  peek() {
-    if (!this.size) return undefined;
-    return this.buffer[(this.end - 1) & this.mask];
-  }
-
-  /** @param {...T} elem */
-  unshift(...elems) {
-    this.upsize(this.size + elems.length);
-    for (const elem of elems) {
-      this.start = (this.start - 1) & this.mask;
-      this.buffer[this.start] = elem;
+    upsize(target) {
+        while (this.mask < target) {
+            if (this.end <= this.start)
+                this.start += this.mask + 1;
+            this.mask = this.mask << 1 | 1;
+            this.buffer = this.buffer.concat(this.buffer);
+        }
+        this.size = target;
     }
-  }
-
-  /** @return {T|undefined} */
-  shift() {
-    if (!this.size) return undefined;
-    const result = this.buffer[this.start];
-    this.start = (this.start + 1) & this.mask;
-    this.size--;
-    return result;
-  }
-
-  /** @return {T|undefined} */
-  front() {
-    if (!this.size) return undefined;
-    return this.buffer[this.start];
-  }
-
-toString(){
-const parts=new Array(this.size);
-  for (let i = 0; i < this.size; i++)parts[i]=this.buffer[(this.start+i)&this.mask];return '['+parts.join(', ')+']';
+    push(...elems) {
+        this.upsize(this.size + elems.length);
+        for (const elem of elems) {
+            this.buffer[this.end] = elem;
+            this.end = (this.end + 1) & this.mask;
+        }
+    }
+    pop() {
+        if (!this.size)
+            return undefined;
+        this.end = (this.end - 1) & this.mask;
+        this.size--;
+        return this.buffer[this.end];
+    }
+    peek() {
+        if (!this.size)
+            return undefined;
+        return this.buffer[(this.end - 1) & this.mask];
+    }
+    unshift(...elems) {
+        this.upsize(this.size + elems.length);
+        for (const elem of elems) {
+            this.start = (this.start - 1) & this.mask;
+            this.buffer[this.start] = elem;
+        }
+    }
+    shift() {
+        if (!this.size)
+            return undefined;
+        const result = this.buffer[this.start];
+        this.start = (this.start + 1) & this.mask;
+        this.size--;
+        return result;
+    }
+    front() {
+        if (!this.size)
+            return undefined;
+        return this.buffer[this.start];
+    }
+    toString() {
+        const parts = new Array(this.size);
+        for (let i = 0; i < this.size; i++) {
+            parts[i] = this.buffer[(this.start + i) & this.mask];
+        }
+        return `[${parts.join(', ')}]`;
+    }
 }
-}
-
-// /** @template T */
-// export class DequeSet {
-//   constructor() {
-//     /** @type {!Array<T|undefined>} */
-//     this.buffer = new Array(16);
-//     /** @type {number} */
-//     this.mask = 0xf;
-//     /** @type {number} */
-//     this.start = 0;
-//     /** @type {number} */
-//     this.end = 0;
-//     /** @type {number} */
-//     this.size = 0; // readonly externally
-//     /** @type {!Set<T>} */
-//     this.set = new Set();
-//   }
-
-//   upsize(target) {
-//     while (this.mask < target) {
-//       this.start += this.mask + 1;
-//       this.mask = this.mask << 1 | 1;
-//       this.buffer = this.buffer.concat(this.buffer);
-//     }
-//     this.size = target;
-//   }
-
-//   /** @param {...T} elem */
-//   push(...elems) {
-//     this.upsize(this.size + elems.length);
-//     for (const elem of elems) {
-//       if (this.set.has(elem)) {
-//         this.size--;
-//         continue;
-//       }
-//       this.buffer[this.end] = elem;
-//       this.end = (this.end + 1) & this.mask;
-//     }
-//   }
-
-//   /** @return {T|undefined} */
-//   pop() {
-//     if (!this.size) return undefined;
-//     this.end = (this.end - 1) & this.mask;
-//     this.size--;
-//     const out = this.buffer[this.end];
-//     this.set.delete(out);
-//     return out;
-//   }
-
-//   /** @return {T|undefined} */
-//   peek() {
-//     if (!this.size) return undefined;
-//     return this.buffer[(this.end - 1) & this.mask];
-//   }
-
-//   /** @param {...T} elem */
-//   unshift(...elems) {
-//     this.upsize(this.size + elems.length);
-//     for (const elem of elems) {
-//       if (this.set.has(elem)) {
-//         this.size--;
-//         continue;
-//       }
-//       this.start = (this.start - 1) & this.mask;
-//       this.buffer[this.start] = elem;
-//     }
-//   }
-
-//   /** @return {T|undefined} */
-//   shift() {
-//     if (!this.size) return undefined;
-//     const result = this.buffer[this.start];
-//     this.start = (this.start + 1) & this.mask;
-//     this.size--;
-//     this.set.remove(result);
-//     return result;
-//   }
-
-//   /** @return {T|undefined} */
-//   front() {
-//     if (!this.size) return undefined;
-//     return this.buffer[this.start];
-//   }
-// }
-
-export class IndexedList {
-  constructor() {
-    this.list = [];
-    this.map = new Map();
-  }
-
-  add(elem) {
-    if (this.map.has(elem)) return;
-    this.map.set(elem, this.list.length);
-    this.list.push(elem);
-  }
-
-  indexOf(elem) {
-    return this.map.get(elem);
-  }
-
-  remove(elem) {
-    // TODO - this isn't super efficient...
-    // We could maintain a small handful of split points.
-    // Or a RemovalTree where it starts with a fully-balanced
-    // binary tree (height ~ log(n)) and then we just remove
-    // elements from there so that we only need to update
-    // O(log(n)) "size" values on the way up.  Though this
-    // doesn't help to actually *find* the element...
-    // Another option would be to use the bits of the index
-    // to keep track of the number of removed elements before.
-    // So we have a same-size array of numbers
-    // where each entry tells the size to add for the Nth one-bit
-    // and all the higher bits.
-    //   00 -> 0
-    //   01 -> 1
-    //   10 -> 2
-    //   11 -> 3 = 2 + 1
-    // Storing
-    //   X#  -> 2
-    //   1X  -> 1
-    //   0X  -> 1
-    // For bigger list,
-    //   11X -> 1    stored at    111 = 7
-    //   10X -> 1                 110 = 6
-    //   01X -> 1                 101 = 5
-    //   00X -> 1                 100 = 4
-    //   1X# -> 2                 011 = 3
-    //   0X# -> 2                 010 = 2
-    //   X## -> 4                 001 = 1
-    // The upshot is that when removing an element we only need to
-    // update O(log(n)) elements...
-    // And we can avoid splicing the list and even find the first
-    // element with binary search - O(log(n))
-    const index = this.map.get(elem);
-    if (index == null) return;
-    this.list.splice(index, 1);
-    this.map.delete(elem);
-    for (let i = index; i < this.list.length; i++) {
-      this.map.set(this.list[i], i);
-    }
-  }
-
-  [Symbol.iterator]() {
-    return this.list[Symbol.iterator]();
-  }
-}
+//# sourceMappingURL=util.js.map
--- defrag/js/version.js
+++ defrag/js/version.js
@@ -1,5 +1,5 @@
 export const STATUS = 'unstable';
 export const VERSION = 'defrag';
-export const LABEL = 'defrag 1525959';
-export const HASH = '15259590af50500047e2f7b7153d63fb6d94e130';
-export const DATE = new Date(1556448749000);
+export const LABEL = 'defrag 5f299f2';
+export const HASH = '5f299f28ac0e13e2d91fa563cd54bd0eb05803f6';
+export const DATE = new Date(1556449989000);

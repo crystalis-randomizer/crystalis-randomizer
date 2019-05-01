9a3547d8bf09c2c49d9195e51b73c42cac43c2a2
Travis CI User <travis@example.org>
2019-05-01 18:20:04 +0000

    Release defrag

--- defrag/js/version.js
+++ defrag/js/version.js
@@ -1,6 +1,6 @@
 export const STATUS = 'unstable';
 export const VERSION = 'defrag';
-export const LABEL = 'defrag 08203a1';
-export const HASH = '08203a170827c097efa6c752d52205619ca83871';
-export const DATE = new Date(1556734647000);
+export const LABEL = 'defrag c110c89';
+export const HASH = 'c110c89933f8baec5f0163b10b45130cb9bbce77';
+export const DATE = new Date(1556734773000);
 //# sourceMappingURL=version.js.map

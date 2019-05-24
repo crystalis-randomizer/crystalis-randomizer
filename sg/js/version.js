c7c1e6686ff13d10f6d4b9c6942ab6d4fa330c25
Travis CI User <travis@example.org>
2019-05-24 10:19:59 +0000

    Release sg

---
+++ sg/js/version.js
@@ -0,0 +1,5 @@
+export const STATUS = 'rc';
+export const VERSION = 'sg';
+export const LABEL = 'sg a0d332e';
+export const HASH = 'a0d332e27d66e9fe935ba047d8482810b27946c6';
+export const DATE = new Date(1558693178000);

dbe28932c901a54170690f80cc58352e3d2282c3
Travis CI User <travis@example.org>
2019-06-21 08:59:41 +0000

    Release latest

--- latest/js/version.js
+++ latest/js/version.js
@@ -1,6 +1,6 @@
 export const STATUS = 'unstable';
 export const VERSION = 'latest';
-export const LABEL = 'latest 6c8e16c';
-export const HASH = '6c8e16cd85d1ad2c1e05d48ac8815049670e166b';
-export const DATE = new Date(1561107428000);
+export const LABEL = 'latest a8f32dd';
+export const HASH = 'a8f32dd6731595f890117d083a8d79d2f64e613a';
+export const DATE = new Date(1561107532000);
 //# sourceMappingURL=version.js.map

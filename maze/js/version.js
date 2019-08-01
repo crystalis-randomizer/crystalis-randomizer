02159eddf89173f49db1a10302b16ff8ffd4cb5e
Travis CI User <travis@example.org>
2019-08-01 10:39:42 +0000

    Release maze

--- maze/js/version.js
+++ maze/js/version.js
@@ -1,6 +1,6 @@
 export const STATUS = 'unstable';
 export const VERSION = 'maze';
-export const LABEL = 'maze 7e7d76c';
-export const HASH = '7e7d76c841ddb06414f81f049b72c1ff9b0396e6';
-export const DATE = new Date(1564568391000);
+export const LABEL = 'maze 7d8fd6b';
+export const HASH = '7d8fd6b4e843d794db8db1312f3f31e9ffba7304';
+export const DATE = new Date(1564655943000);
 //# sourceMappingURL=version.js.map

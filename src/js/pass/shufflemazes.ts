df63bcda8b7a5d1f285bb847715f186f2d1d9bb9
Stephen Hicks <stephenhicks@gmail.com>
2019-08-03 04:25:19 -0700

    add a quick flatMap polyfill for node 8 and older browsers

--- src/js/pass/shufflemazes.ts
+++ src/js/pass/shufflemazes.ts
@@ -581,3 +581,22 @@ function allSame<T>(arr: T[],
   }
   return true;
 }
+
+// PROVIDE FLATMAP POLYFILL?
+if (!Array.prototype.flatMap) {
+  Object.defineProperties(Array.prototype, {
+    flatMap: {
+      value<T, U>(this: Array<T>, f: (x: T, i: number) => U[]): U[] {
+        const out = [];
+        let i = 0;
+        for (const x of this) {
+          let y = f(x, i++);
+          if (typeof y[Symbol.iterator] !== 'function') y = [y] as any;
+          y = [...y];
+          if (y.length) out.push(...y);
+        }
+        return out;
+      },
+    },
+  });
+}

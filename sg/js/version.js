bd6c14029fd5160b5c58a83911cb6ee2db39ffad
Stephen Hicks <stephenhicks@gmail.com>
2019-08-21 23:13:14 -0700

    fix up permalink versions

--- sg/js/version.js
+++
@@ -1,5 +0,0 @@
-export const STATUS = 'rc';
-export const VERSION = 'sg';
-export const LABEL = 'sg e159b9e';
-export const HASH = 'e159b9e0c0b9c77fc4fe2bc8bdbd8876ced44926';
-export const DATE = new Date(1558693561000);

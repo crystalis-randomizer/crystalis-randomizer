5fcff0bde71bc2183a248f5d5b2f75adf9676800
Travis CI User <travis@example.org>
2019-10-17 09:40:56 +0000

    Release 1.2.0-rc6

---
+++ 1.2.0-rc6/js/externs.js
@@ -0,0 +1,15 @@
+/** @fileoverview @externs */
+
+/** @constructor @return {!BigInt} */
+function BigInt(/** number */ num) {}
+
+const localStorage = {};
+
+/** @param {string} key
+    @return {string} */
+localStorage.getItem = function(key) {};
+
+/** @param {string} key
+    @param {string} value */
+localStorage.setItem = function(key, value) {};
+

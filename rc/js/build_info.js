48a3f1001edabb263dd5c727b2d521662f6b4a88
Travis CI User <travis@example.org>
2019-10-20 08:54:03 +0000

    Release rc 862bec9

--- rc/js/build_info.js
+++ rc/js/build_info.js
@@ -1,8 +1,8 @@
 var __VERSION__ = {
   'STATUS': 'unstable',
   'VERSION': 'rc',
-  'LABEL': 'rc 2c7d1aa',
-  'HASH': '2c7d1aaef929ee5e8e8c71d304f297850ce0cf53',
-  'DATE': new Date(1571305176000),
+  'LABEL': 'rc 862bec9',
+  'HASH': '862bec91b27687e623bf9675b5ddf66293814f4b',
+  'DATE': new Date(1571561605000),
 };
 if (typeof global !== 'undefined') global['__VERSION__'] = __VERSION__;

f4a5b295c4d055e5c27c430c4308d2eb365f3bbe
Stephen Hicks <stephenhicks@gmail.com>
2019-11-05 02:51:55 -0800

    remove unused branches

--- dyna/js/version.js
+++
@@ -1,5 +0,0 @@
-export const STATUS = 'unstable';
-export const VERSION = 'dyna';
-export const LABEL = 'dyna d1423ec';
-export const HASH = 'd1423ec0f4c477ebfcdb8125b09c5a8cb0799a51';
-export const DATE = new Date(1556825160000);
--- rc/js/build_info.js
+++
@@ -1,8 +0,0 @@
-var __VERSION__ = {
-  'STATUS': 'unstable',
-  'VERSION': 'rc',
-  'LABEL': 'rc fb32114',
-  'HASH': 'fb321145bdda43fc3913c5d63f08d6082c689cb5',
-  'DATE': new Date(1572950170000),
-};
-if (typeof global !== 'undefined') global['__VERSION__'] = __VERSION__;

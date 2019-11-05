c295718553b111f5aa0c4e42b4706875e5432a4e
Travis CI User <travis@example.org>
2019-11-05 11:14:30 +0000

    Release 1.1.4

--- 1.1.4/js/build_info.js
+++ 1.1.4/js/build_info.js
@@ -2,7 +2,7 @@ var __VERSION__ = {
   'STATUS': 'stable',
   'VERSION': '1.1.4',
   'LABEL': '1.1.4',
-  'HASH': 'fb321145bdda43fc3913c5d63f08d6082c689cb5',
-  'DATE': new Date(1572950519000),
+  'HASH': '7f297ef1eba65a3aff24bd1e311cd0456a74adc7',
+  'DATE': new Date(1572952433000),
 };
 if (typeof global !== 'undefined') global['__VERSION__'] = __VERSION__;

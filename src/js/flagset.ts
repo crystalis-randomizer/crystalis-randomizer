ccb48a64bf6a506c2a78b7aa1eb1f15d078030b2
Stephen Hicks <stephenhicks@gmail.com>
2019-12-21 21:17:22 -0800

    add elimination presets

--- src/js/flagset.ts
+++ src/js/flagset.ts
@@ -17,57 +17,58 @@ const REPEATABLE_FLAGS: Set<string> = new Set(['S']);
 export const PRESETS: Preset[] = [
   {
     title: 'Casual',
-
     descr: `Basic flags for a relatively easy playthrough.`,
     flags: 'Ds Edmrsx Fw Mr Rp Sc Sk Sm Tab',
   },
   {
     title: 'Intermediate',
-
     descr: `Slightly more challenge than Casual but still approachable.`,
     flags: 'Ds Edms Fsw Gt Mr Ps Rpt Sct Skm Tab',
-
     default: true,
   },
   {
     title: 'Full Shuffle',
-
     descr:
         `Slightly harder than intermediate, with full shuffle and no spoiler log.`,
     flags: 'Em Fsw Gt Mert Ps Rprt Sckmt Tabmp Wmtuw',
   },
   {
     title: 'Glitchless',
-
     descr: `Full shuffle but with no glitches.`,
     flags: 'Em Fcpstw Mert Ps Rprt Sckmt Tab Wmtuw',
   },
   {
     // TODO: add 'Ht' for maxing out tower scaling
     title: 'Advanced',
-
     descr: `A balanced randomization with quite a bit more difficulty.`,
     flags: 'Fsw Gfprt Hbdgw Mert Ps Roprst Sckt Sm Tabmp Wmtuw',
   },
   {
     // TODO: add 'Ht'
     title: 'Ludicrous',
-
     descr: `Pulls out all the stops, may require superhuman feats.`,
     flags: 'Fs Gcfprtw Hbdgmswxz Mert Ps Roprst Sckmt Tabmp Wmtuw',
   },
   {
     title: 'Mattrick',
-
     descr: 'Not for the faint of heart. Good luck...',
     flags: 'Fcprsw Gt Hbdhwx Mert Ps Ropst Sckmt Tabmp Wmtuw',
   },
   // TOURNAMENT PRESETS
   {
     title: 'Tournament: Swiss Round',
-
-    descr: 'Quick-paced full-shuffle flags for Swiss Round of 2019 Tournament',
+    descr: 'Quick-paced full-shuffle flags for Swiss round of 2019 Tournament',
     flags: 'Es Fcprsw Gt Hd Mr Ps Rpt Sckmt Tab',
+  },
+  {
+    title: 'Tournament: Elimination Round',
+    descr: 'More thorough flags for the first elimination rounds of the 2019 Tournament',
+    flags: 'Em Fprsw Gft Hbd Mer Ps Rprst Sckmt Tab Wt',
+  },
+  {
+    title: 'Tournament: Semifinals',
+    descr: 'Advanced flags for semifinal round of the 2019 Tournament',
+    flags: 'Em Fsw Gft Hbd Mert Ps Roprst Sckmt Tab Wt',
   },
 ];
 

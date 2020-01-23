e95bfd56a3d491bce019a03c4153ccae15426421
Travis CI User <travis@example.org>
2020-01-23 22:55:26 +0000

    Release rc e6548f8

--- rc/js/build_info.js
+++ rc/js/build_info.js
@@ -1,8 +1,8 @@
 var __VERSION__ = {
   'STATUS': 'unstable',
   'VERSION': 'rc',
-  'LABEL': 'rc a22b038',
-  'HASH': 'a22b0388a74a4a735cde58e8647f9d755ada83f4',
-  'DATE': new Date(1579500367000),
+  'LABEL': 'rc e6548f8',
+  'HASH': 'e6548f8ffb48113000d5fb270712cc64befaab8b',
+  'DATE': new Date(1579820093000),
 };
 if (typeof global !== 'undefined') global['__VERSION__'] = __VERSION__;

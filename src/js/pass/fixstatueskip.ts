559fd5bec17fccb643b170b49ac2c8407b0cec04
Stephen Hicks <stephenhicks@gmail.com>
2020-05-10 03:30:58 -0700

    Fix tower monsters
    Make sure we don't move them around yet.

---
+++ src/js/pass/fixstatueskip.ts
@@ -0,0 +1,6 @@
+// Basic plan to fix these:
+//  1. object fd gets its 480 increased from 0b to 12
+//  2. find all the monster spawns w/ id 3f (8f)
+//     - if xt > 8, increase by 1; if < 8, decrease by 1
+
+// New plan: just change fd's hitbox from c1 to ce
--- src/js/pass/shufflemonsterpositions.ts
+++ src/js/pass/shufflemonsterpositions.ts
@@ -3,6 +3,7 @@ import {Rom} from '../rom.js';
 
 export function shuffleMonsterPositions(rom: Rom, random: Random) {
   for (const loc of rom.locations) {
+    if ((loc.id & 0xf8) === 0x58) continue; // skip tower
     loc.meta.replaceMonsters(random);
   }
 }

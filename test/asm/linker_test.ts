crystalis-randomizer/crystalis-randomizer

commit 458c9fbd7360d438087ccc77f0a4eda624f9d7df

move most free routines to .reloc

src/js/asm/linker.ts
@@ -44,6 +44,10 @@ export class Linker {
     if (this._exports) return this._exports;
     return this._exports = this._link.buildExports();
   }
+
+  watch(...offset: number[]) {
+    this._link.watches.push(...offset);
+  }
 }
 
 export namespace Linker {
@@ -169,6 +173,9 @@ class LinkChunk {
     this._org = org;
     this._segment = segment;
     const offset = this._offset = org + segment.delta;
+    for (const w of this.linker.watches) {
+      if (w >= offset && w < offset + this.size) debugger;
+    }
     // Copy data, leaving out any holes
     const full = this.linker.data;
     const data = this._data ?? fail(`No data`);
@@ -348,6 +355,8 @@ class Link {
 
   resolvedChunks: LinkChunk[] = [];
   unresolvedChunks = new Set<LinkChunk>();
+
+  watches: number[] = []; // debugging aid: offsets to watch.
 
   // TODO - deferred - store some sort of dependency graph?
 

src/js/asm/assembler.ts
@@ -7,8 +7,6 @@ import {assertNever} from '../util.js';
 
 type Chunk = mod.Chunk<number[]>;
 type Module = mod.Module;
-type Segment = mod.Segment;
-const Segment = mod.Segment;
 
 class Symbol {
   /**
@@ -135,7 +133,7 @@ export class Assembler {
   private segments: readonly string[] = ['code'];
 
   /** Data on all the segments. */
-  private segmentData = new Map<string, Segment>();
+  private segmentData = new Map<string, mod.Segment>();
 
   /** Stack of segments for .pushseg/.popseg. */
   private segmentStack: Array<readonly [readonly string[], Chunk?]> = [];
@@ -386,7 +384,7 @@ export class Assembler {
       if (symbol.export != null) out.export = symbol.export;
       symbols.push(out);
     }
-    const segments: Segment[] = [...this.segmentData.values()];
+    const segments: mod.Segment[] = [...this.segmentData.values()];
     return {chunks, symbols, segments};
   }
 
@@ -695,13 +693,13 @@ export class Assembler {
     this._name = name;
   }
 
-  segment(...segments: Array<string|Segment>) {
+  segment(...segments: Array<string|mod.Segment>) {
     // Usage: .segment "1a", "1b", ...
     this.segments = segments.map(s => typeof s === 'string' ? s : s.name);
     for (const s of segments) {
       if (typeof s === 'object') {
         const data = this.segmentData.get(s.name) || {name: s.name};
-        this.segmentData.set(s.name, Segment.merge(data, s));
+        this.segmentData.set(s.name, mod.Segment.merge(data, s));
       }
     }
     this._chunk = undefined;
@@ -750,10 +748,16 @@ export class Assembler {
 
   free(size: number, token?: Token) {
     // Must be in .org for a single segment.
-    if (this.segments.length !== 1) {
+    if (this._org == null) this.fail(`.free in .reloc mode`, token);
+    const segments = this.segments.length > 1 ? this.segments.filter(s => {
+      const data = this.segmentData.get(s);
+      if (!data || data.memory == null || data.size == null) return false;
+      if (data.memory > this._org!) return false;
+      if (data.memory + data.size <= this._org!) return false;
+      return true;
+    }) : this.segments;
+    if (segments.length !== 1) {
       this.fail(`.free with non-unique segment: ${this.segments}`, token);
-    } else if (this._org == null) {
-      this.fail(`.free in .reloc mode`, token);
     } else if (size < 0) {
       this.fail(`.free with negative size: ${size}`, token);
     }
@@ -763,7 +767,7 @@ export class Assembler {
     }
     this._chunk = undefined;
     // Ensure a segment object exists.
-    const name = this.segments[0];
+    const name = segments[0];
     let s = this.segmentData.get(name);
     if (!s) this.segmentData.set(name, s = {name});
     (s.free || (s.free = [])).push([this._org, this._org + size]);
@@ -825,7 +829,7 @@ export class Assembler {
     this.currentScope = this.currentScope.parent;
   }
 
-  pushSeg(...segments: Array<string|Segment>) {
+  pushSeg(...segments: Array<string|mod.Segment>) {
     this.segmentStack.push([this.segments, this._chunk]);
     this.segment(...segments);
   }
@@ -865,7 +869,7 @@ export class Assembler {
     return str;
   }
 
-  parseSegmentList(tokens: Token[], start = 1): Array<string|Segment> {
+  parseSegmentList(tokens: Token[], start = 1): Array<string|mod.Segment> {
     if (tokens.length < start + 1) {
       this.fail(`Expected a segment list`, tokens[start - 1]);
     }
@@ -875,7 +879,7 @@ export class Assembler {
       if (!Token.eq(ts[1], Token.COLON)) {
         this.fail(`Expected comma or colon: ${Token.name(ts[1])}`, ts[1]);
       }
-      const seg = {name: str} as Segment;
+      const seg = {name: str} as mod.Segment;
       // TODO - parse expressions...
       const attrs = Token.parseAttrList(ts, 1); // : ident [...]
       for (const [key, val] of attrs) {

src/js/asm/define.ts
@@ -99,7 +99,11 @@ function produce(tokens: Token[],
       overflow.push(line = []);
       continue;
     }
-    line.push(tok);
+    const source =
+        tok.source && tokens[0].source ?
+            {...tok.source, parent: tokens[0].source} :
+            tok.source || tokens[0].source;
+    line.push(source ? {...tok, source} : tok);
   }
   overflow = overflow.filter(l => l.length);
   if (overflow.length && end < tokens.length) {

src/js/asm/macro.ts
@@ -80,7 +80,11 @@ export class Macro {
             mapped.push({token: 'grp', inner: map(tok.inner)});
             continue;
           }
-          mapped.push(tok);
+          const source =
+              tok.source && tokens[0].source ?
+                  {...tok.source, parent: tokens[0].source} :
+                  tok.source || tokens[0].source;
+          mapped.push(source ? {...tok, source} : tok);
         }
         return mapped;
       }

src/js/asm/token.ts
@@ -165,7 +165,9 @@ export namespace Token {
 
   export function at(arg: {source?: SourceInfo}): string {
     const s = arg.source;
-    return s ? `\n  at ${s.file}:${s.line}:${s.column}` : '';
+    if (!s) return '';
+    const parent = s.parent ? at({source: s.parent}) : '';
+    return `\n  at ${s.file}:${s.line}:${s.column}${parent}`;
     // TODO - definition vs usage?
   }
 

src/js/init.s
@@ -1,0 +1,251 @@
+.define FREE {seg [start, end)} \
+    .pushseg seg .eol \
+    .org start .eol \
+    .free end - start .eol \
+    .popseg
+.define FREE {seg [start, end]} .noexpand FREE seg [start, end + 1)
+
+.macro FREE_UNTIL end
+  .assert * <= end
+  .free end - *
+.endmacro
+
+.macro MOVE bytes, segment, source
+  .local S
+  .pushseg segment
+  .org source
+  S = *
+  .popseg
+  .move bytes, S
+.endmacro
+
+.segment "00"   :bank $00 :size $2000 :off $00000 :mem $8000
+.segment "01"   :bank $01 :size $2000 :off $02000 :mem $a000
+.segment "02"   :bank $02 :size $2000 :off $04000 :mem $8000
+.segment "03"   :bank $03 :size $2000 :off $06000 :mem $a000
+.segment "04"   :bank $04 :size $2000 :off $08000 :mem $8000
+.segment "05"   :bank $05 :size $2000 :off $0a000 :mem $a000
+.segment "06"   :bank $06 :size $2000 :off $0c000 :mem $8000
+.segment "07"   :bank $07 :size $2000 :off $0e000 :mem $a000
+.segment "08"   :bank $08 :size $2000 :off $10000 :mem $8000
+.segment "09"   :bank $09 :size $2000 :off $12000 :mem $a000
+.segment "0a"   :bank $0a :size $2000 :off $14000 :mem $8000
+.segment "0b"   :bank $0b :size $2000 :off $16000 :mem $a000
+.segment "0c"   :bank $0c :size $2000 :off $18000 :mem $8000
+.segment "0d"   :bank $0d :size $2000 :off $1a000 :mem $a000
+.segment "0e"   :bank $0e :size $2000 :off $1c000 :mem $8000
+.segment "0f"   :bank $0f :size $2000 :off $1e000 :mem $a000
+.segment "10"   :bank $10 :size $2000 :off $20000 :mem $8000
+.segment "11"   :bank $11 :size $2000 :off $22000 :mem $a000
+.segment "12"   :bank $12 :size $2000 :off $24000 :mem $8000
+.segment "13"   :bank $13 :size $2000 :off $26000 :mem $a000
+.segment "14"   :bank $14 :size $2000 :off $28000 :mem $8000
+.segment "15"   :bank $15 :size $2000 :off $2a000 :mem $a000
+.segment "16"   :bank $16 :size $2000 :off $2c000 :mem $8000
+;;; 15..17 store messages, all accessed via the a000 slot
+.segment "16:a" :bank $16 :size $2000 :off $2c000 :mem $a000
+.segment "17"   :bank $17 :size $2000 :off $2e000 :mem $a000
+.segment "18"   :bank $18 :size $2000 :off $30000 :mem $8000
+.segment "19"   :bank $19 :size $2000 :off $32000 :mem $a000
+.segment "1a"   :bank $1a :size $2000 :off $34000 :mem $8000
+.segment "1b"   :bank $1b :size $2000 :off $36000 :mem $a000
+.segment "1c"   :bank $1c :size $2000 :off $38000 :mem $8000
+.segment "1d"   :bank $1d :size $2000 :off $3a000 :mem $a000
+
+;;; Note: we moved these when we expanded the rom.
+.segment "fe"   :bank $1e :size $2000 :off $3c000 :mem $c000
+.segment "ff"   :bank $1f :size $2000 :off $3e000 :mem $e000
+
+
+;;; Various global definitions.
+ObjectRecoil = $340
+ObjectHP = $3c0
+PlayerHP = $3c1
+PlayerMaxHP = $3c0
+ObjectAtk = $3e0
+PlayerAtk = $3e1
+ObjectDef = $400
+PlayerLevel = $421
+ObjectActionScript = $4a0
+ObjectGold = $500
+ObjectElementalDefense = $500
+ObjectExp = $520
+PlayerMP = $708
+EquippedConsumableItem = $715
+EquippedPassiveItem    = $716
+
+
+InvSwords = $6430
+InvConsumables = $6440
+InvPassive = $6448
+InvQuest = $6450
+InvMagic = $6458
+SlotFlagsStart = $64a0
+ItemFlagsStart = $64c0
+Difficulty = $648f         ; requires defrag! (flags 078 .. 07f)
+ShouldRedisplayDifficulty = $61ff
+
+        
+SelectedConsumableIndex = $642c
+SelectedQuestItemIndex  = $642e
+
+.ifdef _EXTRA_PITY_MP
+PITY_MP_AMOUNT    = 34
+.else
+PITY_MP_AMOUNT    = 1
+.endif        
+
+PITY_HP_AMOUNT    = 5
+
+SHOP_COUNT        = 11
+SCALING_LEVELS    = 48
+
+.ifdef _UNIDENTIFIED_ITEMS
+SORT_START_ROW    = 3
+.else
+SORT_START_ROW    = 2
+.endif
+
+;;; Constants
+ITEM_RABBIT_BOOTS    = $12
+ITEM_OPEL_STATUE     = $26
+SFX_MONSTER_HIT      = $21
+SFX_ATTACK_IMMUNE    = $3a
+
+;;; see http://www.6502.org/tutorials/6502opcodes.html#BIT
+;;; note: this is dangerous if it would result in a register read
+.define SKIP_TWO_BYTES .byte $2c
+
+;;; Labels (TODO - consider keeping track of bank?)
+.segment "0e"                   ; 1c000
+SetOrClearFlagsFromBytePair_24y = $8112
+ReadFlagFromBytePair_24y        = $8135
+ItemGet                         = $826f
+ItemGet_Bracelet                = $82f4
+ItemGet_FindOpenSlot            = $8308
+ItemUse_TradeIn                 = $8354
+
+.segment "10"       ; 20000
+Shop_NothingPressed = $97cd
+AfterLoadGame       = $9c7a
+
+.segment "13"      ; 26000
+PlayerDeath        = $b91c
+ActivateOpelStatue = $b9b0
+
+
+.segment "1a"         ; 34000
+ArmorDefense          = $8bc0
+ShieldDefense         = $8bc9
+DisplayNumberInternal = $8e46
+KillObject            = $9152
+KnockbackObject       = $95c0
+
+.segment "fe"              ; 3c000
+PowersOfTwo                = $c000
+UpdateEquipmentAndStatus   = $c008
+StartAudioTrack            = $c125
+LoadOneObjectDataInternal  = $c25d
+FlushNametableDataWrite    = $c676
+MainLoop_01_Game           = $cab6
+CheckForPlayerDeath        = $cb84
+DialogAction_11            = $d21d
+LoadAndShowDialog          = $d347
+WaitForDialogToBeDismissed = $d354
+SpawnMimic                 = $d3da
+MainLoopItemGet            = $d3ff
+
+.segment "ff"                 ; 3e000
+RestoreBanksAndReturn         = $e756
+ReadControllersWithDirections = $fe80
+DisplayNumber                 = $ffa9
+
+;;; Various free sections
+
+;;; another 256 free in map space
+FREE "0b" [$bf00, $c000)        ; 17f00 .. 18000
+;;; ~80 bytes free in middle of SFX data that could be used on npc data page?
+FREE "0c" [$83fc, $844d)        ; 183fc .. 1844d
+;;; empty space at end of npcdata
+FREE "0d" [$aba3, $ac00)        ; 1aba3 .. 1ac00
+
+;;; 58 bytes of free/unused space at start of itemuse jump
+FREE "0e" [$8399, $83d3)
+;;; 16 bytes of free/unused space in middle of itemuse jump
+FREE "0e" [$83eb, $83fb)
+;;; 30 bytes of free/unused space at end of itemuse jump
+FREE "0e" [$841b, $8439)
+
+;;; TODO - move these into ROM - as we discover they're unused,
+
+;;; free them directly there...
+;;; Free space in middle of spawn condition table (lots of unused IDs)
+FREE "0e" [$86f2, $86fc)
+FREE "0e" [$86fe, $8760)
+
+;;; Free bytes in middle of dialog table
+FREE "0e" [$8a6f, $8a79)
+FREE "0e" [$8a7b, $8ae3)
+
+;;; TreasureChestSpawnTable no longer needed
+
+;;; Also frees the first HALF of the ItemGetTable, which is identity
+;;; This leads to some tricky business, since we end up indexing into
+;;; the table only starting at index $49, so that $9d66 is the base,
+;;; even though that's inside the freed section.
+FREE "0e" [$9c82, $9daf)
+
+;;; Treasure chest spawn flags also unneeded
+FREE "0f" [$a106, $a17a)
+;;; Free space at end of trigger data
+FREE "0f" [$a3c0, $a3f0)
+
+;;; Debug mode (note: requires patch at 1fde4)
+FREE "0f" [$bf46, $c000)
+
+;;; Unused subroutine
+FREE "10" [$8a37, $8a5a)
+;;; Unused space at end of item name table 21471
+FREE "10" [$9471, $9500)
+
+;;; Unused subroutine
+FREE "13" [$a4b3, $a4bf)
+;;; Unused bytes after some of the intro menu graphics
+FREE "13" [$b88d, $b900)
+;;; A few unused bytes at the end of the page
+FREE "13" [$bff2, $c000)
+
+;;; Random unused data table
+FREE "14" [$8520, $8528)
+
+;;; NOTE: there's space here, but we glob it into the space
+;;; recovered from defragging MapData... if we want it back
+;;; we'll need to change the "end" address there.
+;.org $17cfa
+;;; just over 256 bytes free in map space
+;.assert < $17e00
+
+FREE "1b" [$a086, $a0a3)
+
+
+FREE "fe" [$c446, $c482)
+;;; Random uncovered 6 bytes between dialog actions
+FREE "fe" [$d196, $d19c)
+;;; Recovered from item/trigger jump 08 (get paralysis)
+FREE "fe" [$d654, $d659)
+;;; Recovered from other item/trigger jumps (06/11, 0b, 0c, 0d, 0e, 0f,
+FREE "fe" [$d6d5, $d746)
+
+FREE "ff" [$f9ba, $fe00)
+FREE "ff" [$fe01, $fe16)
+FREE "ff" [$fe18, $fe78) ;; NOTE: 3fe2e might be safer than 3fe18
+FREE "ff" [$ff44, $ff80)
+FREE "ff" [$ffe3, $fffa)
+
+;;; Patch the end of ItemUse to check for a few more items.
+.segment "0e"
+.org $834d
+  jmp PatchTradeInItem
+
+.org $8156                      ; 1c157
+  lda PowersOfTwo,x ; no need for multiple copies

src/js/patch.ts
@@ -316,7 +316,8 @@ export async function shuffle(rom: Uint8Array,
   //   asm.assemble(await reader.read(path), path, rom);
   // }
   async function tokenizer(path: string) {
-    return new Tokenizer(await reader.read(path), path);
+    return new Tokenizer(await reader.read(path), path,
+                         {lineContinuations: true});
   }
   const flagFile =
       Object.keys(defines)
@@ -325,6 +326,7 @@ export async function shuffle(rom: Uint8Array,
   const toks = new TokenStream();
   toks.enter(TokenSource.concat(
       new Tokenizer(flagFile, 'flags.s'),
+      await tokenizer('init.s'),
       await tokenizer('preshuffle.s'),
       await tokenizer('postparse.s'),
       await tokenizer('postshuffle.s')));

src/js/postparse.s
@@ -3,10 +3,17 @@
 .segment "0e", "fe", "ff"
 
 .ifdef _NORMALIZE_TELEPATHY
+FREE "0e" [$8167, $822f)
+;FREE "0e" [$98f4, $9b00) -- currently declared in rom/telepathy.ts
+
 ;;; Basic plan: rip out minimum level, result mapping, etc
 ;;; Also removed the extra powers of two table, so we have
 ;;; room to inline CheckTelepathyResult.
-.org $8167
+.org $de14  ; Ref from MainGameModeJump_16 jumped to 816f before
+    jsr CastTelepathy
+
+.reloc
+CastTelepathy:
     sec
     lda PlayerMP
     sbc #$08 ; should never overflow because already checked
@@ -81,15 +88,10 @@ Telepathy_ShowDefaultMessage:
     lda TelepathyTable+1,x
     sta $20
     rts
-.assert * <= $822f
-.org $822f
-TelepathyResults:
+
+TelepathyResults = $822f
 
 .import TelepathyTable
-
-;; .org $98f4
-;; TelepathyTable:
-;;   .skip 32
 
 .endif
 
@@ -101,26 +103,30 @@ TelepathyResults:
 ;;; Initialize tool shop
 .org $98ee
   lda ToolShopIdTable,x
+
 .org $98ff
   clc
   adc #SHOP_COUNT*4 ; 44 = delta between shop tables
   tax
   jsr CopyShopPrices
   jmp PostInitializeShop
-.assert * <= $9912
+FREE_UNTIL $9912
 
 ;;; Initialize armor shop
 .org $9895
   lda ArmorShopIdTable,x ; should be unchanged, but just in case...
+
 .org $98a6
   tax
   jsr CopyShopPrices
   jmp PostInitializeShop
+FREE_UNTIL $98b6
+
+.reloc
 ShopItemHorizontalPositions:
   .byte 8,13,18,23
-.assert * <= $98b6
-.org $98b6
-PostInitializeShop:
+
+PostInitializeShop = $98b6
 
 .org $98bc  ; use the new position table
   lda ShopItemHorizontalPositions,x
@@ -147,6 +153,7 @@ PostInitializeShop:
   nop
   nop
 .assert * = $81cf
+
 ;;; Second version of the same thing (this one happens only
 ;;; once, when you say "yes" to "sell another?").
 .org $84c7
@@ -159,6 +166,7 @@ PostInitializeShop:
   nop
   nop
 .assert * = $84d5
+
 ;;; Third read of price is immediately when selling.
 .org $8634
   sta $61
@@ -200,14 +208,23 @@ PostInitializeShop:
   lsr
   sta $646d  ; current shop index   
   rts
-.assert * <= $9970
+FREE_UNTIL $9970
 
 ;;; These are exported by the Shops writer.
 .import ShopData, ArmorShopIdTable, ToolShopIdTable, ArmorShopPriceTable
 .import ToolShopPriceTable, InnPrices, ShopLocations
 .import ToolShopScaling, ArmorShopScaling, BasePrices, InnBasePrice
 
-.org $9da4 + SHOP_COUNT*21 + SCALING_LEVELS*2 + 54
+
+
+
+
+;;; TODO - probably mark this whole area as freed and dispense
+;;; with the concrete .org math...?!?
+
+FREE "10" [$9da4, $a000]
+
+;; .org $9da4 + SHOP_COUNT*21 + SCALING_LEVELS*2 + 54
 
 ;;; This is the space freed up by compressing the shop tables
 
@@ -220,6 +237,7 @@ PostInitializeShop:
 ;;; Could get 48 or 72 bytes back by densifying it?
 ;;;   -> only scale every 2 or 4 levels...
 
+.reloc
 ComputeShopPrice:               ; ~71 bytes
     ;; Inputs:
     ;;   Difficulty - scaling level
@@ -297,6 +315,7 @@ ComputeShopPrice:               ; ~71 bytes
 
 ;;; NOTE: we could move this to a smaller chunk if needed, but it's nice to
 ;;; store all the shop normalization code in the space it recovered.
+.reloc
 CopyShopPrices:
   ;; Input:
   ;;   x: first item in the shop
@@ -316,6 +335,7 @@ CopyShopPrices:
   bcc -
   rts
 
+.reloc
 Multiply32Bit:
   ;; Inputs: $61$62 and $63$64
   ;; Output: $10$11$12$13
@@ -341,8 +361,6 @@ Multiply32Bit:
   dex
   bne -
   rts
-
-.assert * <= $a000
 
 .endif
 

commit 0995bd4dc185c06485c969c85b540d6b2042a30c

Fix rapid-fire paralysis bug
This was introduced by 458c9fb, which changed ContinuousMagicTable (originally at 36092) to be .reloc, using the new .move directive to copy the data from the original table.  Unfortunately, there was a bug in the implementation of .move where it copied data from the wrong location for non-relative offsets.  The fix is to subtract the chunk's .org in that case.  The effect was that it copied all zeros, effectively making _all_ magics continuous - but since every _other_ noncontinuous magic brings up a menu, it's only observable with paralysis.

src/js/asm/expr.ts
@@ -64,6 +64,7 @@ export namespace Expr {
         return expr;
       case 'num':
         if (expr.meta?.rel && expr.meta.org != null) {
+          // Remove the 'rel' tag since it's no longer relative.
           const {rel, ...meta} = expr.meta;
           // TODO - pull size from meta?
           return {op: 'num', num: expr.num! + meta.org!, meta};

src/js/asm/linker.ts
@@ -274,7 +274,9 @@ class LinkChunk {
       if (sub.expr.args!.length !== 1) throw new Error(`bad .move`);
       const child = sub.expr.args![0];
       if (child.op === 'num' && child.meta?.offset != null) {
-        const start = child.meta!.offset! + child.num!;
+        const delta =
+            child.meta!.offset! - (child.meta!.rel ? 0 : child.meta!.org!);
+        const start = child.num! + delta;
         const slice = this.linker.orig.slice(start, start + sub.size);
         this.writeBytes(sub.offset, Uint8Array.from(slice));
         del = true;

test/asm/linker_test.ts
@@ -279,6 +279,33 @@ describe('Linker', function() {
     expect([...patch.chunks()]).to.eql([[150, [5, 7, 9, 1, 1, 3, 3]]]);
   });
 
+  it('should .move existing data from an absolute offset', function() {
+    const base = Uint8Array.of(
+      // starts at 10
+      0, 2, 4, 6, 8, 0, 2, 4, 6, 8,
+      1, 3, 5, 7, 9, 1, 1, 3, 3, 5,
+      5, 7, 7, 9, 9, 0, 0, 0, 2, 2,
+      2, 4, 4, 4, 6, 6, 6, 8, 8, 8);
+    const m = {
+      chunks: [{
+        segments: ['b'],
+        data: Uint8Array.of(),
+      }, {
+        segments: ['a'],
+        data: Uint8Array.of(0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff),
+        subs: [{offset: 0, size: 7, expr: {op: '.move', args: [
+          {op: 'num', num: 12, meta: {org: 10, offset: 20}},
+        ]}}],
+      }],
+      segments: [{
+        name: 'a', size: 100, offset: 100, memory: 0,
+        free: [[50, 100] as const],
+      }, {name: 'b', size: 40, offset: 0, memory: 0}],
+    };
+    const patch = new Linker().base(base, 10).read(m).link();
+    expect([...patch.chunks()]).to.eql([[150, [5, 7, 9, 1, 1, 3, 3]]]);
+  });
+
   it('should resolve bank bytes', function() {
     const m = {
       chunks: [{
45eabe8a69c817e055f87b8aeb8e739c46d3bacb
Travis CI User <travis@example.org>
2020-07-26 10:44:15 +0000

    Release latest 0995bd4

--- latest/js/build_info.js
+++ latest/js/build_info.js
@@ -1,8 +1,8 @@
 var __VERSION__ = {
   'STATUS': 'unstable',
   'VERSION': 'latest',
-  'LABEL': 'latest 3c54386',
-  'HASH': '3c543861e97665bdd78701703a644d91dc1943f1',
-  'DATE': new Date(1591870999000),
+  'LABEL': 'latest 0995bd4',
+  'HASH': '0995bd4dc185c06485c969c85b540d6b2042a30c',
+  'DATE': new Date(1595760205000),
 };
 if (typeof global !== 'undefined') global['__VERSION__'] = __VERSION__;

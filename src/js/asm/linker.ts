ede9a393f8d0d8f2375a8e3b6a31a7bac5aa70c9
Stephen Hicks <stephenhicks@gmail.com>
2021-01-05 16:44:39 -0800

    slightly improve linker error messages

--- src/js/asm/linker.ts
+++ src/js/asm/linker.ts
@@ -314,7 +314,8 @@ class LinkChunk {
     const bits = (size) << 3;
     if (val != null && (val < (-1 << bits) || val >= (1 << bits))) {
       const name = ['byte', 'word', 'farword', 'dword'][size - 1];
-      throw new Error(`Not a ${name}: $${val.toString(16)}`);
+      throw new Error(`Not a ${name}: $${val.toString(16)} at $${
+          (this.org! + offset).toString(16)}`);
     }
     const bytes = new Uint8Array(size);
     for (let i = 0; i < size; i++) {

65ca7a31052c7a262298900dc232fa0768255f78
Stephen Hicks <stephenhicks@gmail.com>
2021-10-15 16:50:48 -0400

    fix up some of the closed-cave exits

---
+++ src/js/pass/checktriggers.ts
@@ -0,0 +1,15 @@
+import { Rom } from '../rom.js';
+
+/** Checks that there are no triggers on top of each other. */
+export function checkTriggers(rom: Rom) {
+  for (const loc of rom.locations) {
+    const triggers = new Set<number>();
+    for (const spawn of loc.spawns) {
+      if (!spawn.isTrigger()) continue;
+      if (triggers.has(spawn.coord)) {
+        throw new Error(`Overlapping triggers on ${loc} at ${
+                         spawn.coord.toString(16)}`);
+      }
+    }
+  }
+}

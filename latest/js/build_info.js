53ea2c80f16d7afe02ce4df5b34c2df261bab49f
Stephen Hicks <stephenhicks@gmail.com>
2023-02-19 22:56:00 -0500

    fix latest/build_info.js manually

---
+++ latest/js/build_info.js
@@ -0,0 +1,8 @@
+var __VERSION__ = {
+  'STATUS': 'unstable',
+  'VERSION': 'latest',
+  'LABEL': 'latest 48663c5',
+  'HASH': '48663c5783d10920bc6ef840eb131a65a7968a87',
+  'DATE': new Date(1674376251000),
+  'PREV': '3f43aeb',
+};

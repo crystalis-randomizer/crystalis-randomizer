5add62eeb1976a3fd992cd3d1aa80bcedde43577
James Rowe <jroweboy@gmail.com>
2023-07-21 00:48:10 -0400

    Use the sword palette that you just got in the get sword animation

--- src/asm/animation.s
+++ src/asm/animation.s
@@ -4,7 +4,7 @@
 ;;; Patches to animation routines.  Includes
 ;;;  1. Revert from change before sword-raise animation
 
-.segment "13", "fe", "ff"   ; TODO - check 12
+.segment "13" ;
 
 ;;; Fix the graphics glitch from getting a sword while changed.
 .org $bc04
@@ -17,3 +17,23 @@ MaybeRevertChangeOnSwordGet:
   beq +
    jsr $bb9d ; 27b9d MainGameModeJump_19_ChangeMagicRevertAnimation
 + jmp $c867  ; 3c867 ??
+
+.segment "13" ; we are banking $8000 so we can't put the relocated code there
+.org $bc16
+  jsr UpdateSwordPaletteOnSwordGet
+
+.reloc
+UpdateSwordPaletteOnSwordGet:
+  ; a == #0 so just add the value of the new sword we just got +1 (still in most recent item slot)
+  sta $06c0 ; do original patch then set the sword palette
+  sec ; intentionally add 1 since sword palettes start from 1
+  adc $07dc
+  sta $07e5
+  lda $6e
+  pha
+    lda #$1a
+    jsr BankSwitch8k_8000
+    jsr LoadPalettesForLocation ; 34000 loaded at 8000
+  pla
+  jmp BankSwitch8k_8000
+  ; rts ; implicit

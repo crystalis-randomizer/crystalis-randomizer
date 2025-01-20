e672103017d95e5e1dc0655fc47a9a0248f17529
Ars-Ignis <just.a.smith@att.net>
2025-01-20 17:32:08 -0600

    Allow for the user to input a Crystalis ROM dumped from the SNK 40th Anniversary Collection available for purchase on Steam.
    It's literally one bit of difference, so I added a second allowed CRC32, and then added an entry to the static ROM adjustments that will change the 40th anniversary version into the original NES cart version so that the final checksum is the same.
    
    This should have been a merge from snk-40th, but conflict resolution wanted to push ap-patch into snk-40th, which I'm trying to keep clean for the PR, so instead I'm duplicating the work.

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import * as patch from './patch';
 import * as render from './render';
 import * as version from './version';
 import { crc32 } from './crc32';
-import { EXPECTED_CRC32 } from './rom';
+import {EXPECTED_CRC32_NES, EXPECTED_CRC32_SNK_40TH} from './rom.js';
 import { FlagSet } from './flagset';
 import { ProgressTracker } from './progress';
 import { CharacterSet, Sprite, parseNssFile } from './characters';
@@ -457,7 +457,8 @@ const loadRomFromStorage = () => {
   const upload = document.getElementById('pick-file');
   const checkCrc = () => {
     document.body.classList.add('rom-uploaded');
-    document.body.classList.toggle('rom-broken', crc32(rom) != EXPECTED_CRC32);
+    const orig_crc = crc32(rom);
+    document.body.classList.toggle('rom-broken', orig_crc != EXPECTED_CRC32_NES && orig_crc != EXPECTED_CRC32_SNK_40TH);
   };
   // const shuffle = document.getElementById('shuffle');
   // const badcrc = document.getElementById('badcrc');
